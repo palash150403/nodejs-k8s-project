537b507deff23d5a6c2efabae01b37fcc7e565e1
Palash Gajjar <narendramodihe@gmail.com>
2024-08-20 11:11:58 +0530

    added route

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Kubernetes!');
+app.get('/newroute', (req, res) => {
+    res.send('This is a new route! of Palash kubernetes ');
 });
 
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
 });
+
